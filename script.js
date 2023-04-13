57ad1e99150952c7ba5923f95a99be08cfb6317d
unknown <johannkubeja@gmail.com>
2023-04-13 16:03:36 -0700

    Added the prompts for checking if user wants lowercase, uppercase, special characters, and numbers

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+//Assignment code here
+
 // Get reference to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var passwordLength = 12;                                                                                        // Recommended password length
-  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\\:;?><,./-=";
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";                                                  //Added the charecters as different types
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+~`|}{[]\\:;?><,./-="
   var password = "";
 
   passwordLength = prompt("Please enter a password length between 8 and 128 characters:");       // Alert box for passwords length
@@ -15,18 +20,47 @@ function writePassword() {
 
   passwordLength = parseInt(passwordLength);                                       // Convert password length to integer
 
-  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {       // Check if the password length is not a number or not between 8 and 128
+  passwordLength = parseInt(passwordLength);
+
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {                   //Checks for the password length that the user wants
     alert("Please enter a valid number between 8 and 128");
+    return;
+  }
+
+  var includeLowercase = confirm("Do you want to include lowercase characters?");       //Checks for if user wants lower cases
+  var includeUppercase = confirm("Do you want to include uppercase characters?");        //Checks for if user wanter upper cases
+  var includeNumeric = confirm("Do you want to include numeric characters?");         //Checks if the user wants numbers
+  var includeSpecial = confirm("Do you want to include special characters?");      // Checks if the user wants special characters
+
+  var charset = "";
+
+  if (includeLowercase) {                                                                 // functions for each prompt screen
+    charset += lowercaseChars;
+  }
+
+  if (includeUppercase) {
+    charset += uppercaseChars;
+  }
+
+  if (includeNumeric) {
+    charset += numericChars;
+  }
+
+  if (includeSpecial) {
+    charset += specialChars;
+  }
+
+  if (charset === "") {
+    alert("Please select at least one character type");
     return;
   }
 
   for (var i = 0; i < passwordLength; i++) {
     password += charset.charAt(Math.floor(Math.random() * charset.length));
   }
-
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
