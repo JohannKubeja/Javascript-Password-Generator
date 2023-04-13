bf295f4ff1b36b69370bea7c4b34c9785f6033a6
unknown <johannkubeja@gmail.com>
2023-04-13 15:14:59 -0700

    Added to Html and Javascript to add functionality to the button. Also, added a alert box to make the user enter how long they would like the password that will be generated.

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Get reference to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var passwordLength = 12;                                                                                        // Recommended password length
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\\:;?><,./-=";
+  var password = "";
+
+  passwordLength = prompt("Please enter a password length between 8 and 128 characters:");       // Alert box for passwords length
+
+  if (passwordLength === null || passwordLength === "") {                                // Has the user has pressed cancel
+    return;
+  }
+
+  passwordLength = parseInt(passwordLength);                                       // Convert password length to integer
+
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {       // Check if the password length is not a number or not between 8 and 128
+    alert("Please enter a valid number between 8 and 128");
+    return;
+  }
+
+  for (var i = 0; i < passwordLength; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
